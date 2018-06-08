d000c002b1417380f5fb75ad64f7b6ca10acdbbf
blacktiger54 <blacktigeroff@mail.ru>
2018-06-08 14:01:27 +0300

    inital commit

---
+++ memeserv/js.js
@@ -0,0 +1,213 @@
+
+var _delay = 0.1;
+
+function createTag(name,url,delay,tSize) {
+
+	$('#sideBar').append('<div class="listLine" style="transition-delay: '+_delay+'s, 0s, 0s;"><div class="listLine-d" style="font-size: '+tSize+'px;" id="text"><span>'+name+'</span></div></div>');
+	_delay+=0.05;
+	//$(elem).css('transition-delay', delay+'s');
+	//img/icons/STICKER ICO.png
+}
+createTag(
+	"все",
+	["пошел","пошёл","иди","на","нахуй","нахер","нахрен","хуй",""],
+	30);
+
+createTag(
+	"пошел нахуй",
+	["пошел","пошёл","иди","на","нахуй","нахер","нахрен","хуй",""],
+	30);
+
+createTag(
+	"сложна",
+	["сложна","сложно","надо","не","нинад","нахер",""],
+	30);
+
+createTag(
+	"поясни",
+	["поясни","всм","всмысле","всмычле","как","нахрен",""],
+	30);
+
+createTag(
+	"ржомба",
+	["смишно","очень","смешно","бля","ржомба",""],
+	30);
+
+createTag(
+	"чо бля?",
+	["че","чо","шо","бля","а",""],
+	30);
+
+createTag(
+	"охуенно!",
+	["охуенно","заебись","отлично","хорошо","ура",""],
+	30);
+
+createTag(
+	"опятьдрочить",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"подозрительно",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"triggered",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"поздравляю",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"suicide",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"ебать",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"уважаемо",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"секс",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+createTag(
+	"разное",
+	["опять","дрочить","секс","красиво","жара",""],
+	30);
+
+
+
+// createProject("ssticker","img/icons/STICKER ICO.png","",0.3);
+// createProject("sticker","img/icons/STICKER ICO.png","",0.4);
+
+var sideBar = false;
+var info = false;
+
+function destroy(elem) {
+	$(elem).removeAttr("style");
+}
+
+function refresh_UI() {
+	if (sideBar) {
+		$('#sideBar').css('left', '0px');
+		$('.listLine').css('left', '0px');
+		
+		$('#mainBody').css('left', '53%');
+		$('body').css('background-position-x', '16px');
+	}else{
+		$('#sideBar').css('left', '-258px');
+		$('.listLine').css('left', '-267px');
+		$('#mainBody').css('left', '50%');
+		$('body').css('background-position-x', '0px');
+	}
+}
+
+function turnSideBar() {
+	if (!sideBar) {
+		sideBar = true;
+		refresh_UI();
+
+	}else{
+		sideBar = false;
+		refresh_UI();
+	}
+}
+
+$('body').on('mousedown', '.listLine', function(e) {
+	e.preventDefault();
+	// for (var i = 0; i < $('.memeCont').ht; i++) {
+	// 	$('.memeCont')[i].
+	// }
+	$('.memeCont').html('');
+	console.log(e);
+
+	if ($(e.target).find('span').html()=="все" || $(e.target).parent().find('span').html()=="все") {
+		for (var i = 0; i < memeses.length; i++) {
+			if (memeses[i].src!="") {
+				$('.memeCont').append('<div class="meme"><a target="_blank" href="'+memeses[i].src+'"><img id="memeImg" src="'+memeses[i].src+'"></a></div>');
+			}
+		}
+	}else{
+		for (var i = 0; i < memeses.length; i++) {
+
+			if (memeses[i].tag == $(e.target).find('span').html() || memeses[i].tag == $(e.target).parent().find('span').html() && memeses[i].src!="") {
+				$('.memeCont').append('<div class="meme"><a target="_blank" href="'+memeses[i].src+'"><img id="memeImg" src="'+memeses[i].src+'"></a></div>');
+			}
+		}
+	}
+});
+
+
+$('body').on('mousedown', '*[data-url="projects"]', function(e) {
+	e.preventDefault();
+	$(this).css('transform', 'scale(0.9)');
+
+	setTimeout(destroy($(this)), 500);
+
+
+	// $(this).removeAttr("style");
+
+	if (!sideBar) {
+		sideBar = true;
+		refresh_UI();
+
+	}else{
+		sideBar = false;
+		refresh_UI();
+	}
+});
+
+$('body').on('mousedown', '*[data-url="info"]', function(e) {
+	e.preventDefault();
+	$(this).css('transform', 'scale(0.9)');
+
+	setTimeout(destroy($(this)), 500);
+
+
+	// $(this).removeAttr("style");
+	if (!info) {
+		$('#sideBar').css('left', '-258px');
+		$('.listLine').css('left', '-267px');
+		$('#mainBody').css('left', '50%');
+		$('body').css('background-position-x', '0px');
+		$('#mainBody').css('top', '-54%');
+		info = true;
+	}else{
+		$('body').css('background-position-y', '0px');
+		$('#mainBody').css('top', '0%');
+		info = false;
+		refresh_UI();
+	}
+});
+
+
+
+
+
+$('#title').css('top', '0px');
+
+$('#page-image').css('margin-top', '0px');
+$('.page-scrn').css('bottom', '0px');
+$('#right-align').css('height', '405px');
+$('#backButton').css('left', '0px');
+
+//8px
+
+function update() {
+	var y = window.scrollY;
+	$('body').css('background-position-y', y+'px');
+}
+
---
+++ memeserv/mememeses.js
@@ -0,0 +1,83 @@
+var memeses = [];
+var num = 0;
+
+function addMeme(tag,src) {
+	memeses[num] = {};
+	memeses[num].tag = tag;
+	memeses[num].src = src;
+	num+=1;
+}
+
+addMeme("пошел нахуй","https://pp.userapi.com/c846019/v846019595/6f346/3N1JTg8xKmg.jpg");
+addMeme("опятьдрочить","https://pp.userapi.com/c846419/v846419139/4246a/ToMTNPs2ubM.jpg");
+addMeme("подозрительно","https://pp.userapi.com/c841028/v841028153/72c2a/BP1krMnuTko.jpg");
+addMeme("triggered","https://pp.userapi.com/c841338/v841338189/49021/-MgUeCsN_0Y.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c831508/v831508626/a783e/Yco9o2JDXV8.jpg");
+addMeme("опятьдрочить","https://pp.userapi.com/c830708/v830708787/7fe2e/74xt2ErBQEA.jpg");
+addMeme("сложна","https://pp.userapi.com/c830708/v830708787/7fe2e/74xt2ErBQEA.jpg");
+addMeme("охуенно!","https://sun1-7.userapi.com/c840622/v840622029/2fb74/tMJpbETKtK4.jpg");
+addMeme("поздравляю","https://pp.userapi.com/c840338/v840338882/6133d/_6lG0w6W3MI.jpg");
+addMeme("сложна","https://pp.userapi.com/c840233/v840233192/88e7c/23-qwmmoZrI.jpg");
+addMeme("suicide","https://pp.userapi.com/c841433/v841433002/7b6d2/IfNgdcPp50o.jpg");
+addMeme("ржомба","https://pp.userapi.com/c846320/v846320506/2cbbc/igjg8Ca0gQc.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c834302/v834302967/123f64/naFKOyCTITY.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c830209/v830209176/eaa22/_gqzYH4xopA.jpg");
+addMeme("поясни","https://pp.userapi.com/c830209/v830209176/eaa22/_gqzYH4xopA.jpg");
+addMeme("опятьдрочить","https://pp.userapi.com/c846121/v846121967/3ab75/bqgs3og59-4.jpg");
+addMeme("ебать","https://pp.userapi.com/c845020/v845020545/4137e/SPGKVU9SxQE.jpg");
+addMeme("ржомба","https://pp.userapi.com/c845020/v845020545/4137e/SPGKVU9SxQE.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c543107/v543107808/4b2c2/9UkH2ItAW_A.jpg");
+addMeme("уважаемо","https://pp.userapi.com/c7003/v7003169/48d36/rwuHsvBGQbg.jpg");
+addMeme("секс","https://pp.userapi.com/c830408/v830408668/f0cb5/esW6EHMEckw.jpg");
+addMeme("охуенно!","https://pp.userapi.com/c846320/v846320257/63e3e/jqK9i46VAzg.jpg");
+addMeme("сложна","https://pp.userapi.com/c846320/v846320257/63e3e/jqK9i46VAzg.jpg");
+addMeme("секс","https://sun1-5.userapi.com/c840731/v840731606/8e149/YgKXYSJznW4.jpg");
+addMeme("опятьдрочить","https://pp.userapi.com/c847120/v847120868/5e43c/H25Hg2csnrA.jpg");
+addMeme("пошел нахуй","http://turboportal.net/uploads/posts/2013-07/1374175032_nahui-poshel.jpg");
+addMeme("чо бля?","https://i.ytimg.com/vi/umhMcgTnIig/hqdefault.jpg");
+addMeme("ебать","https://pp.userapi.com/c831108/v831108918/10fb48/323fBtDUt5c.jpg");
+addMeme("секс","https://pp.userapi.com/c841539/v841539260/681b5/53ZH-jPa5Hk.jpg");
+addMeme("охуенно!","https://pp.userapi.com/c841539/v841539625/4d9d/0UPCebLMtzc.jpg");
+addMeme("секс","https://sun1-1.userapi.com/c840724/v840724355/76b59/RSpE8ecr-Z0.jpg");
+addMeme("охуенно!","https://pp.userapi.com/c831309/v831309726/1129d5/Prx9CafKLZ0.jpg");
+addMeme("suicide","https://pp.userapi.com/c834400/v834400726/15dcff/HA4rwcFI42w.jpg");
+addMeme("suicide","https://pp.userapi.com/c834404/v834404916/164299/NpsTOoHqyV8.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c824502/v824502344/ca034/iMoErsXeiY4.jpg");
+addMeme("поясни","https://sun1-19.userapi.com/c840528/v840528059/867aa/V7ieOoUdIxs.jpg");
+addMeme("сложна","https://sun1-19.userapi.com/c840528/v840528059/867aa/V7ieOoUdIxs.jpg");
+addMeme("секс","https://pp.userapi.com/c824501/v824501868/14cacc/wQ8F18bpLP4.jpg");
+addMeme("охуенно","https://pp.userapi.com/c824501/v824501868/14cacc/wQ8F18bpLP4.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c7002/v7002568/47fb9/L7T9lLQroMU.jpg");
+addMeme("пошел нахуй","https://pp.userapi.com/c7002/v7002568/47fb9/L7T9lLQroMU.jpg");
+addMeme("пошел нахуй","https://pp.userapi.com/c834301/v834301112/140787/AT3mlxsyx1k.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c834301/v834301112/140787/AT3mlxsyx1k.jpg");
+addMeme("ржомба","https://pp.userapi.com/c845220/v845220126/5b0b2/EaU_ZIZdrSo.jpg");
+addMeme("ржомба","https://pp.userapi.com/c847124/v847124916/5404d/wK3qMGVYxpk.jpg");
+addMeme("пошел нахуй","https://pp.userapi.com/c824411/v824411839/120be5/udYkgpKtyc8.jpg");
+addMeme("разное","https://pp.userapi.com/c847123/v847123577/4a915/Bsa61d-o2VY.jpg");
+addMeme("пошел нахуй","https://sun1-5.userapi.com/c840531/v840531577/6ef0e/QKaavvO5gTU.jpg");
+addMeme("разное","https://pp.userapi.com/c844321/v844321022/53ceb/jE0cNCmFSus.jpg");
+addMeme("разное","https://pp.userapi.com/c847018/v847018022/4e553/RY3Ytqkm_Fo.jpg");
+addMeme("ебать","https://pp.userapi.com/c824700/v824700503/135c90/klxAxCIhgtU.jpg");
+addMeme("сложна","https://pp.userapi.com/c824700/v824700503/135c90/klxAxCIhgtU.jpg");
+addMeme("секс","https://pp.userapi.com/c845416/v845416831/4de08/FZr3EzumOeQ.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c840336/v840336424/7ec97/dOJ7wnDSikM.jpg");
+addMeme("охуенно!","https://pp.userapi.com/c840336/v840336424/7ec97/dOJ7wnDSikM.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c543100/v543100861/3307a/B6MAXu4eV84.jpg");
+addMeme("ржомба","https://pp.userapi.com/c840125/v840125084/4528e/gnMVVx8X7fc.jpg");
+addMeme("разное","https://pp.userapi.com/c840125/v840125084/4528e/gnMVVx8X7fc.jpg");
+addMeme("подозрительно","https://pp.userapi.com/c837738/v837738854/347d1/YWMTytfHa2o.jpg");
+addMeme("чо бля?","https://pp.userapi.com/c844320/v844320815/462f2/VPCBUYWepN8.jpg");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+addMeme("опятьдрочить","");
+
+
+
